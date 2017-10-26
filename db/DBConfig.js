195f59e92eb3f3ef5d028b930d65dc33b77a7fa4
崔晋瑜 <2510271615@qq.com>
2017-10-26 19:02:43 +0800

    Update DBConfig.js

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -2,7 +2,7 @@ module.exports={
     mysql:{
         host:'127.0.0.1',
         user:'root',
-        password:'cuijinyu123456789.',
+        password:'',
         database:'smellnote',
         port:3306
     }
